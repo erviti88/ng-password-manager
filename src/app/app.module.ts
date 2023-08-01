5ea6c6d1f43e8dd79d18279fb104ef2c6151d94c
erviti88 <ignacio.erviti@bootcamp-upgrade.com>
2023-08-01 04:02:55 +0200

    Instalacion proyecto base

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SiteListComponent } from './components/site-list/site-list.component';
+import { PasswordListComponent } from './components/password-list/password-list.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SiteListComponent,
+    PasswordListComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'ng-password-manager-app',
+    appId: '1:1089470565204:web:cc88be7e2c44301ce0d6c1',
+    storageBucket: 'ng-password-manager-app.appspot.com',
+    apiKey: 'AIzaSyBdNURo2tApaCCLEE95abZELA3OMeRK_SI',
+    authDomain: 'ng-password-manager-app.firebaseapp.com',
+    messagingSenderId: '1089470565204',
+  },
+    production: false
+}
